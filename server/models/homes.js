830fc2d9063e72d7b3772f5a540b96fbe7c70c4e
Stefano Chiesa <base278@gmail.com>
2017-10-14 06:39:14 +0800

    Basic homes model

---
+++ server/models/homes.js
@@ -0,0 +1,61 @@
+var fs = require('fs'); 
+var csvParse = require('csv-parse');
+const dataPath = 'data/redfin.csv';
+
+const csvFieldMap = {
+  'SALE TYPE': 'sale_type',
+  'PROPERTY TYPE': 'property_type',
+  'ADDRESS': 'address',
+  'CITY': 'city',
+  'STATE': 'state',
+  'ZIP': 'zip',
+  'PRICE': 'price',
+  'BEDS': 'bed_count',
+  'BATHS': 'bath_count',
+  'LOCATION': 'location',
+  'SQUARE FEET': 'square_feet',
+  'LOT SIZE': 'lot_size',
+  'YEAR BUILT': 'year_built',
+  'DAYS ON MARKET': 'days_on_market',
+  '$/SQUARE FEET': 'price_per_square_feet',
+  'HOA/MONTH': 'hoa_per_month',
+  'STATUS': 'status',
+  'NEXT OPEN HOUSE START TIME': 'next_open_start_timestamp',
+  'NEXT OPEN HOUSE END TIME': 'next_open_end_timestamp',
+  'URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)': 'url',
+  'SOURCE': 'source',
+  'MLS#': 'mls_id',
+  'LATITUDE': 'lat',
+  'LONGITUDE': 'lng'
+};
+
+var homes = {
+  list: [],
+  addHome: function(home) {
+    this.list.push(home);
+  },
+  getAll: function() {
+    return this.list;
+  }
+};
+
+fs.createReadStream(dataPath)
+  .pipe(
+    csvParse({
+      auto_parse: true,
+      auto_parse_date: true,
+      columns: true
+    })
+  )
+  .on('data', function(data) {
+    var home = {}
+    Object.keys(csvFieldMap)
+      .forEach(csvField => {
+        const field = csvFieldMap[csvField];
+        home[field] = data[csvField];
+      });
+
+    homes.addHome(home);       
+  });
+
+module.exports = homes;
--- server/routes/homes.js
+++ server/routes/homes.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var homes = require('../models/homes');
 
 router.get('/', function(req, res, next) {
-  res.json({});
+  res.json({ results: homes.getAll() });
 });
 
 module.exports = router;
