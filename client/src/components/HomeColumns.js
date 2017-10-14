624691147635aa50a5403fe8f42bae5fcb95edbd
Stefano Chiesa <base278@gmail.com>
2017-10-15 02:25:50 +0800

    Infinite scroll

--- client/src/components/HomeColumns.js
+++ client/src/components/HomeColumns.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import Columns from 'grommet/components/Columns';
 import Heading from 'grommet/components/Heading';
 import Section from 'grommet/components/Section';
+import InfiniteScroll from 'react-infinite-scroller';
 import HomeCard from './HomeCard';
 
 class HomeColumns extends Component {
+  ENTRIES_PER_PAGE = 6;
+
+  state = {
+    currentPage: 0,
+  }
+
   renderNoResult() {
     return (
       <Section>
@@ -15,6 +22,24 @@ class HomeColumns extends Component {
     );
   }
 
+  onLoadMorePage = () => {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+  }
+
+  maximumHomesToLoad() {
+    return (this.state.currentPage + 1) * this.ENTRIES_PER_PAGE;
+  }
+
+  hasMorePage() {
+    return this.maximumHomesToLoad() < this.props.homes.length;
+  }
+
+  homesToRender() {
+    return this.props.homes.slice(0, this.maximumHomesToLoad());
+  }
+
   render() {
     if (this.props.homes.length === 0) {
       return this.renderNoResult();
@@ -22,12 +47,18 @@ class HomeColumns extends Component {
 
     return (
       <Section>
-        <Columns maxCount={3}>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.onLoadMorePage}
+          hasMore={this.hasMorePage()}
+        >
+          <Columns maxCount={3} masonry={true}>
           {
-            this.props.homes.map(
+            this.homesToRender().map(
               home => <HomeCard key={home.mls_id} {...home}/>)
           }
-        </Columns>
+          </Columns>
+        </InfiniteScroll>
       </Section>
     );
   }
