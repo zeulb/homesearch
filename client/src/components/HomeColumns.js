5d4f120b6dadb3e031500afe6fbba3d8f3022470
Stefano Chiesa <base278@gmail.com>
2017-10-15 02:12:39 +0800

    Show no results found when no results

--- client/src/components/HomeColumns.js
+++ client/src/components/HomeColumns.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import Columns from 'grommet/components/Columns';
+import Heading from 'grommet/components/Heading';
 import Section from 'grommet/components/Section';
 import HomeCard from './HomeCard';
 
 class HomeColumns extends Component {
+  renderNoResult() {
+    return (
+      <Section>
+        <Heading tag="h2" align="center">
+          No matching homes found.
+        </Heading>
+      </Section>
+    );
+  }
+
   render() {
+    if (this.props.homes.length === 0) {
+      return this.renderNoResult();
+    }
+
     return (
       <Section>
         <Columns maxCount={3}>
